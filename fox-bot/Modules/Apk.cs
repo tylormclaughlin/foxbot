880e5de610fb6f0aa73a2c617da01d1314d559c4
Tylor <mclaughlintylor@gmail.com>
2018-03-20 14:56:35 -0400

    Modified !apk command - Experimenting with CodeHollow.FeedReader nuget package

--- fox-bot/Modules/Apk.cs
+++ fox-bot/Modules/Apk.cs
@@ -3,17 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using CodeHollow.FeedReader;
 
 namespace foxbot.Modules
 {
     public class Apk : ModuleBase<SocketCommandContext>
     {
         [Command("apk")]
-        public async Task ApkAsync(string versionNumber)
+        public async Task ApkAsync(string versionNumber = "")
         {
-            versionNumber = versionNumber.Replace(".", "-");
+            var feed = FeedReader.Read("https://www.apkmirror.com/apk/niantic-inc/pokemon-go/feed/");
 
-            await ReplyAsync($"https://www.apkmirror.com/apk/niantic-inc/pokemon-go/pokemon-go-{versionNumber}-android-apk-download/download/");
+            if (versionNumber == "")
+            {
+                foreach(var item in feed.Items)
+                {
+                    Console.WriteLine(item.Title + " - " + item.Link);
+                }
+            }
+
+
+            //versionNumber = versionNumber.Replace(".", "-");
+
+            //await ReplyAsync($"https://www.apkmirror.com/apk/niantic-inc/pokemon-go/pokemon-go-{versionNumber}-android-apk-download/download/");
         }
     }
 }
