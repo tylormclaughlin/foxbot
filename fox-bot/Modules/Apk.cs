7963a0a5e0e6ace1948b389a82a0a96806f64d44
Tylor <mclaughlintylor@gmail.com>
2018-03-20 16:19:24 -0400

    Modified !apk command - Added RSS feed implementation - Basic error handling if specified version can't be found

--- fox-bot/Modules/Apk.cs
+++ fox-bot/Modules/Apk.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using CodeHollow.FeedReader;
+using System.Linq;
 
 namespace foxbot.Modules
 {
@@ -12,20 +13,32 @@ namespace foxbot.Modules
         [Command("apk")]
         public async Task ApkAsync(string versionNumber = "")
         {
-            var feed = FeedReader.Read("https://www.apkmirror.com/apk/niantic-inc/pokemon-go/feed/");
+            var feed = FeedReader.Read("https://www.apkmirror.com/apk/niantic-inc/pokemon-go/feed/").Items;
+            bool validVersion = false;
 
-            if (versionNumber == "")
+            //Need to do this because the FeedReader.Items collection doesn't have an indexer defined, making accessing specific items impossible without putting them in a list first.
+            foreach(var item in feed)
             {
-                foreach(var item in feed.Items)
+                if (versionNumber != "")
                 {
-                    Console.WriteLine(item.Title + " - " + item.Link);
+                    if (item.Title.Contains(versionNumber))
+                    {
+                        validVersion = true;
+                        await ReplyAsync(item.Title + " - " + item.Link.Replace("-release/", "-android-apk-download/download/"));
+                        break;
+                    }
+                }
+                else
+                {
+                    await ReplyAsync(item.Title + " - " + item.Link.Replace("-release/", "-android-apk-download/download/"));
+                    break;
                 }
             }
 
-
-            //versionNumber = versionNumber.Replace(".", "-");
-
-            //await ReplyAsync($"https://www.apkmirror.com/apk/niantic-inc/pokemon-go/pokemon-go-{versionNumber}-android-apk-download/download/");
+            if (!validVersion && versionNumber != "")
+            {
+                await ReplyAsync("Sorry, I could find that version.");
+            }
         }
     }
 }
