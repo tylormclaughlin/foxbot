83927e4dcdc07bab33f9abf3c25ccef2365085d9
Tylor <mclaughlintylor@gmail.com>
2018-03-20 17:48:01 -0400

    Modified !apk command - Added some input validation to Aok command class

--- fox-bot/Modules/Apk.cs
+++ fox-bot/Modules/Apk.cs
@@ -15,29 +15,38 @@ namespace foxbot.Modules
         {
             var feed = FeedReader.Read("https://www.apkmirror.com/apk/niantic-inc/pokemon-go/feed/").Items;
             bool validVersion = false;
+            string[] splits = versionNumber.Split(".");
 
-            //Need to do this because the FeedReader.Items collection doesn't have an indexer defined, making accessing specific items impossible without putting them in a list first.
-            foreach(var item in feed)
+            //Try to make sure given version number was in a valid format (X.XX)
+            if (splits.Length < 2 || (splits.Length < 3 && splits[1] == ""))
             {
-                if (versionNumber != "")
+                await ReplyAsync("Sorry, I don't think that's a valid version number");
+            }
+            else
+            {
+                //Need to do this because the FeedReader.Items collection doesn't have an indexer defined, making accessing specific items impossible without putting them in a list first.
+                foreach (var item in feed)
                 {
-                    if (item.Title.Contains(versionNumber))
+                    if (versionNumber != "")
                     {
-                        validVersion = true;
+                        if (item.Title.Contains(versionNumber))
+                        {
+                            validVersion = true;
+                            await ReplyAsync(item.Title + " - " + item.Link.Replace("-release/", "-android-apk-download/download/"));
+                            break;
+                        }
+                    }
+                    else
+                    {
                         await ReplyAsync(item.Title + " - " + item.Link.Replace("-release/", "-android-apk-download/download/"));
                         break;
                     }
                 }
-                else
-                {
-                    await ReplyAsync(item.Title + " - " + item.Link.Replace("-release/", "-android-apk-download/download/"));
-                    break;
-                }
-            }
 
-            if (!validVersion && versionNumber != "")
-            {
-                await ReplyAsync("Sorry, I could find that version.");
+                if (!validVersion && versionNumber != "")
+                {
+                    await ReplyAsync("Sorry, I could find that version.");
+                }
             }
         }
     }
