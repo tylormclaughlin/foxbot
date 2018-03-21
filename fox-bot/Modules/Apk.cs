a379550500ffba1d3b32511493e98fe9e0a5d608
Tylor <mclaughlintylor@gmail.com>
2018-03-20 21:33:02 -0400

    Extended !apk command - More simple error handling and validation

--- fox-bot/Modules/Apk.cs
+++ fox-bot/Modules/Apk.cs
@@ -5,47 +5,63 @@ using System.Text;
 using System.Threading.Tasks;
 using CodeHollow.FeedReader;
 using System.Linq;
+using Discord;
 
 namespace foxbot.Modules
 {
     public class Apk : ModuleBase<SocketCommandContext>
     {
         [Command("apk")]
-        public async Task ApkAsync(string versionNumber = "")
+        public async Task DefaultApkAsync(string versionNumber = "")
         {
             var feed = FeedReader.Read("https://www.apkmirror.com/apk/niantic-inc/pokemon-go/feed/").Items;
             bool validVersion = false;
             string[] splits = versionNumber.Split(".");
 
-            //Try to make sure given version number was in a valid format (X.XX)
-            if (splits.Length < 2 || (splits.Length < 3 && splits[1] == ""))
+            //Return a list of all APK versions in the feed.
+            if (versionNumber == "list")
             {
-                await ReplyAsync("Sorry, I don't think that's a valid version number");
+                EmbedBuilder eb = new EmbedBuilder();
+
+                foreach (FeedItem item in feed)
+                {
+                    eb.AddField(item.Title, item.Link.Replace("-release/", "-android-apk-download/download/"));
+                }
+
+                await ReplyAsync("", false, eb);
             }
             else
             {
-                //Need to do this because the FeedReader.Items collection doesn't have an indexer defined, making accessing specific items impossible without putting them in a list first.
-                foreach (var item in feed)
+                //Try to make sure given version number was in a valid format (X.XX)
+                if (versionNumber != "" && (splits.Length < 2 || (splits.Length < 3 && splits[1] == "")))
                 {
-                    if (versionNumber != "")
+                    await ReplyAsync("Sorry, I don't think that's a valid version number");
+                }
+                else
+                {
+                    //Need to do this because the FeedReader.Items collection doesn't have an indexer defined, making accessing specific items impossible without putting them in a list first.
+                    foreach (var item in feed)
                     {
-                        if (item.Title.Contains(versionNumber))
+                        if (versionNumber != "")
                         {
-                            validVersion = true;
+                            if (item.Title.Contains(versionNumber))
+                            {
+                                validVersion = true;
+                                await ReplyAsync(item.Title + " - " + item.Link.Replace("-release/", "-android-apk-download/download/")); //Change the link to point at the direct download
+                                break;
+                            }
+                        }
+                        else
+                        {
                             await ReplyAsync(item.Title + " - " + item.Link.Replace("-release/", "-android-apk-download/download/"));
                             break;
                         }
                     }
-                    else
-                    {
-                        await ReplyAsync(item.Title + " - " + item.Link.Replace("-release/", "-android-apk-download/download/"));
-                        break;
-                    }
-                }
 
-                if (!validVersion && versionNumber != "")
-                {
-                    await ReplyAsync("Sorry, I could find that version.");
+                    if (!validVersion && versionNumber != "")
+                    {
+                        await ReplyAsync("Sorry, I could find that version.");
+                    }
                 }
             }
         }
