562d5a00291336263f579cffd27c26c574d9262c
Tylor <mclaughlintylor@gmail.com>
2018-03-30 11:42:38 -0400

    Added classes for monitoring reactions to messages

---
+++ fox-bot/Modules/ReactionMonitoring.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foxbot.Modules
+{
+    public class ReactionMonitoring : ModuleBase<SocketCommandContext>
+    {
+        [Command("monitor")]
+        public async Task MonitorMessageAsync(int id = 0)
+        {
+            var messages = await Context.Channel.GetMessagesAsync(Context.Message.Id, Direction.Before, 1).Flatten();
+
+            IMessage msgToMonitor = messages.FirstOrDefault();
+
+            await ReplyAsync(msgToMonitor.Id.ToString());
+        }
+    }
+}
---
+++ fox-bot/MonitorList.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace foxbot
+{
+    public class MonitorList
+    {
+        private ConcurrentBag<SocketUserMessage> monitorList = new ConcurrentBag<SocketUserMessage>();
+
+        //Getter
+
+        //Setter
+
+    }
+}
