ba2a5730c1a79af71a70a53c6009e601143c8c09
Tylor <mclaughlintylor@gmail.com>
2019-08-12 18:23:54 -0400

    Fixed bug with editcom not properly recreating the command being edited

---
+++ fox-bot/Modules/Keyword.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foxbot.Modules
+{
+    [Group("keyword")]
+    public class Keyword : ModuleBase<SocketCommandContext>
+    {
+        [Command]
+        [RequireOwner]
+        public async Task KeywordAsync(string keyword)
+        {
+
+        }
+    }
+}
